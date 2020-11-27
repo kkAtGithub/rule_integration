c055dd82d2097e44bed98c3e91605a90f93d8341
kk <44739720+kkAtGithub@users.noreply.github.com>
2020-11-27 16:52:19 +0800

    Update rule_integrator.py

--- rule_integrator.py
+++ rule_integrator.py
@@ -1,21 +1,17 @@
 import os
 from urllib.request import Request, urlopen
 
-
-
 RESULT = ''
-# HC_PING_URL = 'https://hc-ping.com/$TOKEN$'
 
 
 def read_list(file_name_2b):
     result = ''
-    count = 0
     hostname = {}
 
     with open(f'list_2B/{file_name_2b}', mode='r', encoding='UTF-8') as list_2b:
         for list_url in list_2b.readlines():
             list_url = list_url.strip()
-            if not list_url.startswith('#'):
+            if not ((list_url.__len__() == 0) or list_url.startswith('#')):
                 try:
                     headers = {
                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
@@ -31,6 +27,8 @@ def read_list(file_name_2b):
                             if file_name_2b.startswith('Filter_2B'):
                                 line_list = line_list.replace('AdBlock', 'REJECT').replace(' ', '')
                                 line_list = line_list.replace('reject', 'REJECT').replace(' ', '')
+                                if not (line_list.startswith('DOMAIN') or line_list.startswith('DOMAIN-SUFFIX') or line_list.startswith('DOMAIN-KEYWORD') or line_list.startswith('USER-AGENT') or line_list.startswith('IP-CIDR') or line_list.startswith('HOST') or line_list.startswith('HOST-SUFFIX') or line_list.startswith('HOST-KEYWORD')):
+                                    continue
                             if file_name_2b.startswith('Rewrite_2B'):
                                 if line_list.startswith('hostname = '):
                                     line_hostname = line_list[10:line_list.__len__()].strip()
@@ -41,38 +39,29 @@ def read_list(file_name_2b):
                             if line_list in result:
                                 continue
                             else:
-                                count = count + 1
                                 result = f'{result}\n{str(line_list)}'
                     result = f'{result}\n'
                 except Exception as e:
-                    log_fail = f'Error: Fail to get rule from {list_url} \n {e} '
-#                    requests.post(f'{HC_PING_URL}/fail', data=log_fail)
+                    print(f'Error: Fail to get rule from {list_url} \n {e} ')
     if len(hostname) > 0:
         result_hostname = 'hostname = '
         for entry_hostname in hostname.keys():
             result_hostname = f'{result_hostname}{entry_hostname},'
         result_hostname = result_hostname.rstrip(',')
         result = f'{result_hostname}\n{result}'
-    return f'{result}\n', count
+    return f'{result}\n'
 
 
 if __name__ == '__main__':
-    log = ''
-#    HC_PING_URL.replace('$TOKEN$', sys.argv[1])
     list_2b_dir_input = os.walk(f'list_2B')
     if not os.path.exists(f'result'):
         os.mkdir(f'result')
-#    requests.get(f'{HC_PING_URL}/start', timeout=10)
     for path, dir_list, file_list in list_2b_dir_input:
         for file_name in file_list:
             os.system(f'echo {file_name}')
             if file_name.endswith('.url'):
-                RESULT, count = read_list(file_name)
+                RESULT = read_list(file_name)
                 file_name_new = file_name[0:file_name.index('_')] + '_integrated.list'
                 with open(f'result/{file_name_new}', mode='w', encoding='UTF-8') as results:
                     results.write(RESULT)
                     results.flush()
-                log = f'{log}{file_name_new} : {count} \n'
-#    requests.post(f'{HC_PING_URL}', data=log)
-
-
